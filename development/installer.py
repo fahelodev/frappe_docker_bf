2fb963f1b75be4b803e6a945d46eb834c1a396aa
fahelodev <fahelo.dev@gmail.com>
2024-03-27 13:19:57 -0300

    ajustados docker container y docker compose para prueba en ambiente prod

--- development/installer.py
+++ development/installer.py
@@ -64,8 +64,8 @@ def get_args_parser():
         "--frappe-repo",
         action="store",
         type=str,
-        help="frappe repo to use, default: https://github.com/frappe/frappe",  # noqa: E501
-        default="https://github.com/frappe/frappe",
+        help="frappe repo to use, default: https://github.com/fahelodev/frappe",  # noqa: E501
+        default="https://github.com/fahelodev/frappe",
     )
     parser.add_argument(
         "-t",
